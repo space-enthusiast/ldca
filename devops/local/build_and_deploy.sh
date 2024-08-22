c4e5f16ab2c2ac8188d2adbfcf51c79628899185
space-enthusiast <jinding0829@gmail.com>
2024-08-22 11:08:03 +0900

    feat: devops local build and deploy script creataed

---
+++ devops/local/build_and_deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+echo "go to project root directory"
+cd ../..
+
+echo "start build the Docker image"
+cd ./chat-api/ldca-chat
+./gradlew buildImage
+# --no-daemon is used to avoid the error of JAVA 21 gradle not able to use the PATH system variable
+# and failing to find the docker command
+./gradlew publishImageToLocalRegistry --no-daemon
+
+cd ../..
+
+cd ./chat-api/ldca-chatroom
+./gradlew buildImage
+# --no-daemon is used to avoid the error of JAVA 21 gradle not able to use the PATH system variable
+# and failing to find the docker command
+./gradlew publishImageToLocalRegistry --no-daemon
+
+echo "end build the Docker image"
+
+cd ../..
+
+docker-compose up --build -d
+
