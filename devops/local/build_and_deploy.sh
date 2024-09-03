4922458a391254c3e83e6491d9cd8605889bbf07
space-enthusiast <jinding0829@gmail.com>
2024-09-03 23:04:27 +0900

    fix: check java installtion & chmod to mac local build script

--- devops/local/build_and_deploy.sh
+++ devops/local/build_and_deploy.sh
@@ -7,8 +7,15 @@ cd ../..
 echo "erase kafka directory"
 rm -rf kafka/data/*
 
+echo "check for java 21 installation"
+if ! [ -x "$(command -v java)" ]; then
+  echo 'Error: java is not installed.' >&2
+  exit 1
+fi
+
 echo "start build the Docker image"
 cd ./chat-api/ldca-chat
+chmod +x gradlew
 ./gradlew buildImage
 # --no-daemon is used to avoid the error of JAVA 21 gradle not able to use the PATH system variable
 # and failing to find the docker command
@@ -17,6 +24,7 @@ cd ./chat-api/ldca-chat
 cd ../..
 
 cd ./chat-api/ldca-chatroom
+chmod +x gradlew
 ./gradlew buildImage
 # --no-daemon is used to avoid the error of JAVA 21 gradle not able to use the PATH system variable
 # and failing to find the docker command
