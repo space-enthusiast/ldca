47c66e587fc4d960c1d7ec1eb054bca4c8c89368
kimseunghyun <incongnito12@gmail.com>
2024-09-03 19:22:11 +0900

    added the chmod +x for the gradlew

--- devops/local/build_and_deploy.sh
+++ devops/local/build_and_deploy.sh
@@ -9,6 +9,7 @@ rm -rf kafka/data/*
 
 echo "start build the Docker image"
 cd ./chat-api/ldca-chat
+chmod +x ./gradlew
 ./gradlew buildImage
 # --no-daemon is used to avoid the error of JAVA 21 gradle not able to use the PATH system variable
 # and failing to find the docker command
@@ -16,6 +17,7 @@ cd ./chat-api/ldca-chat
 
 cd ../..
 
+chmod +x ./gradlew
 cd ./chat-api/ldca-chatroom
 ./gradlew buildImage
 # --no-daemon is used to avoid the error of JAVA 21 gradle not able to use the PATH system variable
